364fbb9e10ff2f6cf6d38b1addebfe2ffa398951
prabakaranr <prabakaranr@syncfusion.com>
2019-09-25 12:39:47 +0530

    Committed.

--- GettingStartedBound/GettingStartedBound/MainPage.xaml.cs
+++ GettingStartedBound/GettingStartedBound/MainPage.xaml.cs
@@ -13,5 +13,19 @@ namespace GettingStartedBound
         {
             InitializeComponent();
         }
+
+        private void TreeView_SelectionChanged(object sender, Syncfusion.XForms.TreeView.ItemSelectionChangedEventArgs e)
+        {
+            if(e.AddedItems.Count > 0)
+            {
+                var item = e.AddedItems[0] as FileManager;
+
+                if(item.SubFiles == null || item.SubFiles.Count == 0)
+                {
+                    this.IsPresented = false;
+                    Text.Text = item.ItemName;
+                }
+            }
+        }
     }
 }
